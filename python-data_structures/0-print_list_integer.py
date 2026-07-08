5d755e4e5c75178c63a27c4a36822cc4b03532e1
kelvinbruce110 <b.mupenzi1@alustudent.com>
2026-07-08 12:55:40 -0700

    Print list of integers

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 def print_list_integer(my_list=[]):
     for number in my_list:
         print("{:d}".format(number))
-numbers = [98, 402, -3, 5]
-print_list_integer(numbers)
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
